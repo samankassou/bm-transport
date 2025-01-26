98b0747872e8db6b3d38b26827d8a9240cb640e1
Foulla <samankassoufoulla@gmail.com>
2025-01-26 23:47:47 +0100

    chore: adjusts coding style

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -1,11 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Country extends Model
+final class Country extends Model
 {
     /** @use HasFactory<\Database\Factories\CountryFactory> */
     use HasFactory;
